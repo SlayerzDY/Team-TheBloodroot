b5690a7f65a9139e658a9ad93e7dd37220b8c1d4
Alexander Spicer <Alexander.Spicer@hotmail.com>
2026-07-04 11:24:01 -0500

    ?

--- BloodrootThePigHunt/Assets/Scripts/gameManager.cs
+++ BloodrootThePigHunt/Assets/Scripts/gameManager.cs
@@ -118,24 +118,19 @@ public class gameManager : MonoBehaviour
     //==========================================================================================
     // Function, Interact Display
     //==========================================================================================
-    public void InteractDisplay(bool isOn)
-    {
-        if (isOn)
-        {
+    public void InteractDisplay(bool isOn) {
+        if (isOn) {
             menuActive = menuInteract;
             menuActive.SetActive(true);
-        }
-        else
-        {
-            if (menuActive != null)
-            {
-                menuActive.SetActive(false);
-                menuActive = null;
+        } else {
+            if (menuActive != null) {
+                if (menuActive == menuInteract) {
+                    menuActive.SetActive(false);
+                    menuActive = null;
+                }
             }
         }
     }
-    //
-
     //==========================================================================================
     // Function, StartNextWave
     //==========================================================================================
