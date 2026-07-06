1179d9b37ee23d30b56f758d6ac76dda82b51529
Kayley Williams <kayleywilli9998@gmail.com>
2026-07-06 15:18:34 -0500

    fixed some merge issues

---
+++ BloodrootThePigHunt/Assets/Scripts/ScreecherAI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ScreecherAI  : MonoBehaviour 
+{
+    [SerializeField] float alertRadius;
+    [SerializeField] float screamCooldown = 5f;
+    float screamTimer;
+    public GameObject screamVFX;
+
+
+     void Update()
+    {
+       
+        if (screamTimer > 0)
+     
+            screamTimer -= Time.deltaTime;
+    }
+
+    void AlertNearby()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, alertRadius);
+        foreach (Collider collider in hits)
+        {   // Only alert enemies that have enemyAI
+            enemyAI ai = collider.GetComponent<enemyAI>();
+            if (ai != null && ai.gameObject != this.gameObject)
+            {
+                ai.Alert(transform.position);
+            }
+        }
+    }
+    void SpawnVFX()
+    {
+        if (screamVFX != null)
+            Instantiate(screamVFX, transform.position, Quaternion.identity);
+    }
+    public bool CanScream()
+    {
+        return screamTimer <= 0f;
+    }
+
+    // Call this when the screecher scream
+    public void Scream()
+    {
+        if(screamTimer > 0)
+        {
+            return;
+        }
+        screamTimer = screamCooldown;
+
+        AlertNearby();
+        SpawnVFX();
+    }
+}
