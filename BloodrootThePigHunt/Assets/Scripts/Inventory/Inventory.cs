f6ac4d831720d4c07f447006c8919d0cce916d68
Alexander Spicer <Alexander.Spicer@hotmail.com>
2026-08-03 22:11:58 -0500

    Starting Inventory Scripts. Player is now able to atleast add Item Data to their inventory.

---
+++ BloodrootThePigHunt/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,92 @@
+//==============================================================================================
+// Using Unity Engine
+//==============================================================================================
+using System.Linq;
+using UnityEngine;
+//==============================================================================================
+// Declare Inventory
+//==============================================================================================
+public class Inventory : MonoBehaviour {
+    //==========================================================================================
+    // Declare Variables
+    //==========================================================================================
+    // Public
+    // Item Inventory Array
+    [Range(1, 100)] [SerializeField] int inventorySize;
+    [SerializeField] ItemStats[] inventoryItems;
+    [Range(1, 100)][SerializeField] float distance = 2.0f;
+    // Private
+    private ItemStats currItem;
+    private ItemStats tempItem;
+    private int currItemAmount;
+    //==========================================================================================
+    // Declare Functions
+    //==========================================================================================
+    // Function, Add Item
+    //------------------------------------------------------------------------------------------
+    public void Start() {
+        // Initialize Inventory
+        inventoryItems = new ItemStats[inventorySize];
+    }
+    //==========================================================================================
+    // Function, Add Item
+    //------------------------------------------------------------------------------------------
+    public void AddItem(ItemStats item) {
+        if (currItemAmount >= inventorySize) {
+            Vector3 frontPosition = transform.position + (transform.forward * distance);
+            Quaternion localRotation = transform.localRotation;
+            Instantiate(item, frontPosition, localRotation);
+            return;
+        }
+        for (int i = 0; i < inventoryItems.Length; i++) {
+            if (inventoryItems[i] == null) {
+                inventoryItems[i] = item;
+                currItemAmount++;
+                break;
+            }
+        }
+    }
+    //==========================================================================================
+    // Function, Remove Item
+    //------------------------------------------------------------------------------------------
+    public void RemoveItem(ItemStats item) {
+
+    }
+    //==========================================================================================
+    // Function, Assign Item
+    //------------------------------------------------------------------------------------------
+
+    //==========================================================================================
+    // Function, Find Item
+    //------------------------------------------------------------------------------------------
+    public ItemStats FindItem(ItemStats item) {
+        foreach (ItemStats searchItem in inventoryItems) {
+            if (searchItem == item) {
+                return searchItem;
+            }
+        }
+        return null;
+    }
+    //==========================================================================================
+    // Function, Check Item
+    //------------------------------------------------------------------------------------------
+    public bool CheckItem(ItemStats item) {
+        foreach (ItemStats searchItem in inventoryItems) {
+            if (searchItem == item) {
+                // Do something
+                return true;
+            }
+        }
+        return false;
+    }
+    //==========================================================================================
+    // Function, Adjust Stack Size
+    //------------------------------------------------------------------------------------------
+    private void AdjustStackSize() {
+
+    }
+    //==========================================================================================
+}
+//==============================================================================================
+// End of Declare Inventory
+//==============================================================================================
