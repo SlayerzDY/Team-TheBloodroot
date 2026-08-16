4c9fb5ef3967df406873460ce2787ff36b122078
Alexander Spicer <Alexander.Spicer@hotmail.com>
2026-08-16 11:17:28 -0500

    Fixed the Juggernaut and the Screecher

--- BloodrootThePigHunt/Assets/Scripts/Enemy AI/enemyAI.cs
+++ BloodrootThePigHunt/Assets/Scripts/Enemy AI/enemyAI.cs
@@ -134,7 +134,7 @@ public class enemyAI : MonoBehaviour, IDamage
         if (isDead)
             return;
 
-        //if (animator != null) { animator.SetFloat("Speed", agent.velocity.magnitude); }
+        if (animator != null) { animator.SetFloat("Speed", agent.velocity.magnitude); }
         animator.SetFloat("Speed", agent.velocity.magnitude / agent.speed, 0.1f, Time.deltaTime);
 
         if (playerInTrigger)
