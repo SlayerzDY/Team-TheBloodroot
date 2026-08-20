f2c8c7fa776e2fb7953ea2d7f60d66e709537403
Alexander Spicer <Alexander.Spicer@hotmail.com>
2026-08-20 17:48:03 -0500

    Screecher is now working with animations.

--- BloodrootThePigHunt/Assets/Scripts/Enemy AI/ScreecherAI.cs
+++ BloodrootThePigHunt/Assets/Scripts/Enemy AI/ScreecherAI.cs
@@ -1,19 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Linq;
 using UnityEngine;
-using System.Collections;
-
+using UnityEngine.AI;
 
 public class ScreecherAI  : MonoBehaviour 
 {
     [SerializeField] float alertRadius;
     [SerializeField] float screamCooldown = 5f;
+    [SerializeField] private AudioClip[] scream;
+    [SerializeField] public NavMeshAgent agent;
+    [Range(0f, 1f)] [SerializeField] float screamVolume;
+    //private Animator animator;
     float screamTimer;
     public GameObject screamVFX;
 
+    private void Start() {
+        //animator = GetComponentInChildren<Animator>();
+    }
+
      void Update()
     {
-       
+        //animator.SetFloat("Speed", agent.velocity.magnitude / agent.speed, 0.1f, Time.deltaTime);
         if (screamTimer > 0)
      
             screamTimer -= Time.deltaTime;
@@ -21,6 +27,7 @@ public class ScreecherAI  : MonoBehaviour
 
     void AlertNearby()
     {
+        //if (animator != null) { animator.SetTrigger("Roar"); }
         Collider[] hits = Physics.OverlapSphere(transform.position, alertRadius);
         foreach (Collider collider in hits)
         {   // Only alert enemies that have enemyAI
@@ -37,8 +44,7 @@ public class ScreecherAI  : MonoBehaviour
             Instantiate(screamVFX, transform.position, Quaternion.identity);
         }    
     }
-    public bool CanScream()
-    {
+    public bool CanScream() {
         return screamTimer <= 0f;
     }
 
@@ -50,7 +56,7 @@ public class ScreecherAI  : MonoBehaviour
             return;
         }
         screamTimer = screamCooldown;
-
+        if (scream.Length > 0) { audioManager.instance.audPlayer.PlayOneShot(scream[UnityEngine.Random.Range(0, scream.Length)], screamVolume); }
         AlertNearby();
         SpawnVFX();
     }
--- BloodrootThePigHunt/Assets/Scripts/LectureCode/playerController.cs
+++ BloodrootThePigHunt/Assets/Scripts/LectureCode/playerController.cs
@@ -4,8 +4,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 //==============================================================================================
